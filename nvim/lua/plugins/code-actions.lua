5dfe0d2aa82d8f96eb1f4cb87b75bf898d4774a5
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2026-03-25 20:55:34 +0200

    feat(nvim): add code actions

---
+++ nvim/lua/plugins/code-actions.lua
@@ -0,0 +1,15 @@
+return {
+  "ckolkey/ts-node-action",
+  -- dependencies = { "nvim-treesitter" },
+  opts = {},
+  keys = {
+    {
+      "<leader>cn",
+      function()
+        require("ts-node-action").node_action()
+      end,
+      mode = "n",
+      desc = "negate; node actions",
+    },
+  },
+}
