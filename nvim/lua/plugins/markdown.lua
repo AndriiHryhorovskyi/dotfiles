729ad13d56a660a3a2f86ff8bfeb8cb53a081816
AndriiHryhorovskyi <a_hryhorovskyi@ukr.net>
2026-04-09 11:32:26 +0300

    chore(nvim): preview md after leaving buffer

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,21 +1,26 @@
+-- settings for markdown-preview.nvim
+vim.g.mkdp_auto_close = 0
+
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  opts = {
-    enabled = true,
-    quote = { repeat_linebreak = true },
-    win_options = {
-      showbreak = { default = "", rendered = "  " },
-      breakindent = { default = false, rendered = true },
-      breakindentopt = { default = "", rendered = "" },
-    },
-    html = {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
       enabled = true,
-      comment = {
-        conceal = false,
+      quote = { repeat_linebreak = true },
+      win_options = {
+        showbreak = { default = "", rendered = "  " },
+        breakindent = { default = false, rendered = true },
+        breakindentopt = { default = "", rendered = "" },
       },
-    },
-    latex = {
-      enabled = true,
+      html = {
+        enabled = true,
+        comment = {
+          conceal = false,
+        },
+      },
+      latex = {
+        enabled = true,
+      },
     },
   },
 }
